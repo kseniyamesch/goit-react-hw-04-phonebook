d93a857b5772319a3ed23d88ac01e5bbdaebd5bd
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2022-06-19 23:04:11 +0300

    Исправляет ошибки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,16 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter';
 
 export function App() {
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts'))
-  );
+
+  const defaultContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
+  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? defaultContacts);
+
   const [filter, setFilter] = useState('');
 
   const formSubmitHandler = data => {
@@ -42,6 +49,7 @@ export function App() {
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
+
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
